bc88544a0e3328b1e94e890e39752891bc06f365
Consuella Moore <consuellamoore123@gmail.com>
2023-05-09 02:20:45 -0500

    fetched movies

--- movie-project.js
+++ movie-project.js
@@ -1,13 +1,48 @@
 'use strict';
 
+// Delay the execution of this code by 5 seconds
+setTimeout(function() {
+    // Code to load or display content goes here
 
-fetch('https://golden-woozy-frog.glitch.me/movies')
+
+function getMovies () {
+return fetch('https://golden-woozy-frog.glitch.me/movies')
     .then(response => response.json())
-    .then(res => console.log(res[0].title))
+    }
+
+getMovies().then( movies => {
+
+    let markup = "";
+    movies.forEach( movie => {
+        console.log(movie)
+        const title = movie.title;
+        const rating = movie.rating;
+        const director = movie.director;
+        const genre = movie.genre;
+
+        markup +=
+            `<div class="card">
+  <img class="card-img-top" src="img/movieposter_en.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h6 class="card-title">Title: ${title}</h6>
+     <br>
+    <h6 class="card-title">Rating: ${rating}</h6>
+     <br>
+    <h6 class="card-title">Genre: ${genre}</h6>
+     <br>
+    <h6 class="card-title">Director: ${director}</h6>
+   
+  </div>
+</div>`;
+
+        $(".movieInfo").html(markup);
+    });
+})
+}, 5000)
 
 
-const createMovie = () => {
-    let
-}
 
-
+// window.addEventListener('load', function() {
+//     var loadingElement = document.getElementById("loading");
+//     loadingElement.style.display = 'none';
+// });
